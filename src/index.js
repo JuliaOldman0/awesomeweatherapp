25d82bf0eda210e4feb31878144c6ecd0718683e
Julia Oldman <78836093+JuliaOldman0@users.noreply.github.com>
2026-03-09 18:09:57 -0400

    added dynamic icons, humidity, wind

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
+  let conditionElement = document.querySelector("#current-condition");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windElement = document.querySelector("#current-wind");
+  let iconElement = document.querySelector("#current-icon");
+
+  let temperature = Math.round(response.data.temperature.current);
+  let city = response.data.city;
+  let condition = response.data.condition.description;
+  let humidity = response.data.temperature.humidity;
+  let wind = Math.round(response.data.wind.speed);
+  let icon = response.data.condition.icon_url;
+
+  cityElement.innerHTML = city;
   temperatureElement.innerHTML = temperature;
+  conditionElement.innerHTML = condition;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${wind} km/h`;
+  iconElement.innerHTML = `<img src="${icon}" alt="${condition}" class="current-icon" />`;
+}
+
+function searchCity(city) {
+  let apiKey = "813odac11b027491ee76f5at4885a44a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
-  let apiKey = "813odac11b027491ee76f5at4885a44a";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayTemperature);
+  searchCity(searchInputElement.value);
 }
 
 function formatDate(date) {
@@ -40,14 +57,14 @@ function formatDate(date) {
     "Saturday",
   ];
 
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
+  return `${days[day]} ${hours}:${minutes}`;
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("#current-date");
+let currentDateElement = document.querySelector("#current-date");
 let currentDate = new Date();
+currentDateElement.innerHTML = formatDate(currentDate);
 
-currentDateELement.innerHTML = formatDate(currentDate);
+searchCity("Paris");
